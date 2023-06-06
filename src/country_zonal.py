1665e3c7329c901080a1be9276870474ff9a9087
bpstewar <ben.gis.stewart@gmail.com>
2023-06-06 09:01:48 -0400

    initial push of zonal calculations

---
+++ src/country_zonal.py
@@ -0,0 +1,252 @@
+import sys, os, importlib, json
+import folium, shapely, rasterio, matplotlib
+
+import contextily as ctx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pandas as pd
+import geopandas as gpd
+
+from rasterio.crs import CRS
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from h3 import h3
+from shapely.geometry import Polygon, Point, mapping
+from shapely.ops import unary_union
+from urllib.request import urlopen
+from tqdm import tqdm
+
+import h3_helper
+import GOSTRocks.rasterMisc as rMisc
+import GOSTRocks.ntlMisc as ntl
+from GOSTRocks.misc import tPrint
+
+def calculate_value(in_shp, zonal_res, h3_level, feat_id, fractional_res=True):
+    ''' tabulate hexabin stats for all bins that intersect shape x
+
+    :param in_shp: shape of boundary to intersect with hexabins
+    :type in_shp: shapely polygon
+    :param zonal_res: DataFrame of h3 results to search through
+    :type zonal_res: pandas.DataFrame
+    :param h3_level: level of h3 grid to search for
+    :type h3_level: int
+    :param fractional_res: should admin intersection with h3 account for fractional overlaps, default is True
+    :type fractional_res: Boolean
+
+    :return: dictionary of results summarized based on type (SUM, MIN, MEAN, MAX)
+    :rtype: Dictionary
+    '''
+    def get_intersection(admin_shp, hex_shp):
+        ''' get fraction of hex_shp that is inside admin_shp
+        '''
+        if admin_shp.contains(hex_shp):
+            return(1)
+        else:
+            return(admin_shp.intersection(hex_shp).area/hex_shp.area)
+    
+    res = {'id':feat_id}
+    if in_shp.geom_type == "Polygon":
+        sel_h3 = h3.polyfill(in_shp.__geo_interface__, h3_level, geo_json_conformant=True)
+    elif in_shp.geom_type == "MultiPolygon":
+        for cPoly in in_shp:
+            temp_h3 = h3.polyfill(cPoly.__geo_interface__, h3_level, geo_json_conformant=True)
+            try:
+                sel_h3 = sel_h3.union(temp_h3)
+            except:
+                sel_h3 = temp_h3
+    if len(sel_h3) > 0:
+        hex_poly = lambda hex_id: Polygon(h3.h3_to_geo_boundary(hex_id, geo_json=True))
+        all_polys = gpd.GeoSeries(list(map(hex_poly, sel_h3)), index=sel_h3, crs="EPSG:4326")
+        all_polys = gpd.GeoDataFrame(all_polys, crs=4326, columns=['geometry'])
+        all_polys['shape_id'] = list(all_polys.index)
+        if fractional_res:
+            all_polys['inter_area'] = all_polys['geometry'].apply(lambda x: get_intersection(in_shp, x))
+        else:
+            all_polys['inter_area'] = 1
+        all_polys = all_polys.merge(zonal_res, on='shape_id')
+        for col in all_polys.columns:        
+            if "SUM" in col:
+                # For sum columns, multiply column by inter_area and sum results
+                cur_val = sum(all_polys[col] * all_polys['inter_area'])
+            elif "MIN" in col:
+                cur_val = all_polys[col].min()
+            elif "MAX" in col:
+                cur_val = all_polys[col].max()
+            elif "MEAN" in col:
+                cur_val = sum(all_polys[col] * all_polys['inter_area'])/sum(all_polys['inter_area'])
+            try:
+                res[col] = cur_val
+            except:
+                pass
+            try:
+                del(cur_val)
+            except:
+                pass
+    else:
+        pass
+    return(res)
+
+def connect_polygons_h3_stats(inA, stats_df, h3_level, id_col, fractional_res=False):
+    ''' merge stats from hexabin stats dataframe (stats_df) with the inA geodataframe
+    
+    :param inA: input boundary dataset
+    :type inA: geopandas.GeoDataFrame
+    :param stats_df: input hexabin stats dataset
+    :type stats_df: pandas.DataFrame
+    :param h3_level: h3 hex level
+    :type h3_level: int
+    :param id_col: column in inA that uniquely identifies rows
+    :type id_col: string
+    
+    :return: pandas dataframe with attached statistics and matching id from id_col
+    :rtype: geopandas.GeoDataFrame
+    '''
+    all_res = []
+    for idx, row in inA.iterrows():               
+        try:
+            all_res.append(calculate_value(row['geometry'], stats_df, h3_level, row[id_col], fractional_res))
+        except:
+            print(f'Error processing {idx}')        
+        
+    return(pd.DataFrame(all_res))
+    
+class country_h3_zonal():
+    ''' Generate h3 grid at prescribed level; intersect with admin boundary; run zonal stats
+    
+        :param iso3: Country ISO3 code
+        :type iso3: string
+        :param adm_bounds: admin boundaries for joining with h3 grid
+        :type adm_bounds: geopandas.GeoDataFrame
+        :param adm_bounds_id: column in adm_bounds used as unique ID
+        :type adm_bounds_id: string
+        :param h3_level: size of h3 grid to create; we suggest starting with 6 or 5 (5 is larger)
+        :type h3_level: int
+    '''
+    def __init__(self, iso3, adm_bounds, adm_bounds_id, h3_level, out_folder):
+        self.iso3 = iso3
+        self.adm_bounds = adm_bounds
+        self.adm_bounds_id = adm_bounds_id
+        self.h3_level = h3_level
+        self.out_folder = out_folder
+        
+        #define output variables
+        self.out_h3_grid = os.path.join(out_folder, f'h3_{h3_level}_grid.geojson')
+        if os.path.exists(self.out_h3_grid):
+            self.h3_cells = gpd.read_file(self.out_h3_grid)
+        self.out_admin = os.path.join(out_folder, 'admin_bounds.geojson')
+        if os.path.exists(self.out_admin):
+            self.adm_bounds_h3 = gpd.read_file(self.out_admin)
+        
+        
+    def generate_h3_grid(self, cols_to_include=[]):
+        ''' Generate the h3 grid and join to the admin boundaries
+            
+            :param cols_to_include: list of columns to include from adm_bounds in joined output
+            :type cols_to_include: list of strings
+        '''
+        selA = self.adm_bounds
+        try:
+            return(self.h3_cells)
+        except: 
+            pass
+        
+        try:
+            h3_cells = self.h3_cells.copy()
+            h3_cells = h3_cells.loc[:,['shape_id','geometry']]
+        except:
+            h3_cells = h3_helper.generate_h3_gdf(self.adm_bounds, self.h3_level)
+        
+        h3_cells['centroid'] = h3_cells['geometry'].apply(lambda x: x.centroid)                
+        h3_centroids = h3_cells.set_geometry('centroid')
+        cols_to_include.append("geometry")
+        cols_to_include.append(self.adm_bounds_id)
+        cols_to_include = list(set(cols_to_include))
+        h3_joined = gpd.sjoin(h3_centroids, selA.loc[:,cols_to_include], how='left')        
+        h3_pivot = pd.pivot_table(h3_joined, index=self.adm_bounds_id, aggfunc={cols_to_include[0]:len})
+        h3_pivot.columns = [*h3_pivot.columns[:-1], 'h3_count']
+        h3_pivot = h3_pivot.reset_index()
+        h3_pivot = selA.loc[:,cols_to_include].merge(h3_pivot, how='left', on=self.adm_bounds_id)
+        
+        
+        self.adm_bounds_h3 = h3_pivot
+        h3_joined = h3_joined.set_geometry("geometry").drop(['centroid'], axis=1)
+        h3_joined = h3_joined.reset_index()
+        self.h3_cells = h3_joined
+        
+        return(h3_joined)
+        
+    def summarize_adm_h3_join(self, verbose=False):
+        ''' Summarize the join between the adm bounds and the h3 grid:
+            1. Number of h3 cells
+            2. Number of adm bounds
+            3. Number of adm bounds with 0 h3 centroids
+            4. Number of adm bounds with 0 - 1 h3 centroids
+            5. Number of adm bounds with 2 - 5 h3 centroids            
+        '''
+        try:
+            inD = self.adm_bounds_h3.copy()
+        except:
+            self.generate_h3_grid()
+            inD = self.adm_bounds_h3.copy()
+        
+        n_h3 = self.h3_cells.shape[0]
+        n_adm = inD.shape[0]
+        n_adm_0 = inD.loc[inD['h3_count'].isna()].shape[0]
+        n_adm_1 = inD.loc[inD['h3_count'] == 1].shape[0]
+        n_adm_2 = inD.loc[(inD['h3_count'] < 6) & (inD['h3_count'] > 1)].shape[0]
+        
+        if verbose:
+            tPrint(f"{self.iso3}: H3 [{n_h3}], ADM [{n_adm}], ADM0 [{n_adm_0}], ADM1 [{n_adm_1}], ADM2 [{n_adm_2}]")
+        return([n_h3, n_adm, n_adm_0, n_adm_1, n_adm_2])
+        
+    def write_output(self, write_h3=True, write_admin=False):
+        ''' write geospatial data to disk
+        
+        '''
+        if write_h3:
+            self.h3_cells.to_file(self.out_h3_grid, driver="GeoJSON")
+        if write_admin:
+            self.adm_bounds_h3.to_file(self.out_admin, driver="GeoJSON")
+
+    def zonal_raster_urban(self, in_raster, urban_raster, resampling_type="nearest", minVal='', rastType='N'):
+        ''' extract raster data from in_raster, urban_raster for selected country, standardize urban_raster to in_raster
+
+            :param in_raster: string path to raster file for calculations
+            :type in_raster: string
+            :param in_raster: string path to urban file tiering calculations
+            :type in_raster: string            
+            :param minVal: minimum value in in_raster to pass to zonal function; everything below is considered 0. Default is no threshold
+            :type minVal: numeric
+            :param rastType: define the input data in the in_raster. Options are N (for numeric, default) or C (categorical)
+            :type rastType: string
+            
+            :return: dictionary of rasterio objects for in_raster and urban_raster
+            :rtype: dictionary of 'in_raster': rasterio.DatasetReader, 'urban_raster': rasterio.DatasetReader                    
+        '''        
+        h3_grid = self.generate_h3_grid()
+        
+        inR = rasterio.open(in_raster)
+        inU = rasterio.open(urban_raster)
+        # Clip inR to extent of country
+        inN, profile1 = rMisc.clipRaster(inR, self.adm_bounds, crop=False)
+        #return((inN, profile1))
+        with rMisc.create_rasterio_inmemory(profile1, inN) as tempR:
+            # Run zonal statistics on in_raster
+            res = rMisc.zonalStats(h3_grid, tempR, rastType=rastType, reProj=True)
+            res = pd.DataFrame(res, columns=["SUM", "MIN", "MAX", "MEAN"])            
+            res['shape_id'] = h3_grid['shape_id'].astype(object)            
+            
+            # Standardize in_urban raster to clippedR
+            outU, profile2 = rMisc.standardizeInputRasters(inU, tempR, resampling_type=resampling_type)
+            # Isolate values in in_raster that are urban
+            inN_urban = inN * (outU > 20)            
+            with rMisc.create_rasterio_inmemory(profile2, inN_urban) as urbanR:
+                resU = rMisc.zonalStats(h3_grid, urbanR, rastType=rastType, reProj=True)
+                resU = pd.DataFrame(resU, columns=["SUM_urban", "MIN_urban", "MAX_urban", "MEAN_urban"])
+                resU = resU.astype(float)
+                resU['shape_id'] = h3_grid['shape_id']
+                
+        res_final = res.merge(resU, on='shape_id')
+        return(res_final)
+            
+                
+        
---
+++ src/h3_helper.py
@@ -0,0 +1,121 @@
+import sys, os, importlib, json
+import folium, shapely, rasterio, matplotlib
+
+import contextily as ctx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pandas as pd
+import geopandas as gpd
+
+from rasterio.crs import CRS
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from h3 import h3
+from shapely.geometry import Polygon, Point, mapping
+from shapely.ops import unary_union
+from urllib.request import urlopen
+from tqdm import tqdm
+
+import GOSTRocks.rasterMisc as rMisc
+import GOSTRocks.ntlMisc as ntl
+from GOSTRocks.misc import tPrint
+
+def generate_h3_gdf(in_gdf, h3_level=7):
+    ''' Generate a GeoDataFrame of h3 grid cells from an input geodataframe
+    
+        :param in_gdf: geodataframe from which to create h3 cells
+        :type in_gdf: geopandas.GeoDataFrame
+    '''
+    try:
+        del final_hexs
+    except:
+        pass
+
+    try:
+        final_hexs = list(h3.polyfill(in_gdf.unary_union.__geo_interface__, h3_level, geo_json_conformant=True))
+    except:
+        for cPoly in tqdm(in_gdf.unary_union):
+            all_hexs = list(h3.polyfill(cPoly.__geo_interface__, h3_level, geo_json_conformant=True))
+            try:        
+                final_hexs = final_hexs + all_hexs
+            except:
+                final_hexs = all_hexs
+
+            final_hexs = list(set(final_hexs))
+
+    hex_poly = lambda hex_id: Polygon(h3.h3_to_geo_boundary(hex_id, geo_json=True))
+    all_polys = gpd.GeoSeries(list(map(hex_poly, final_hexs)), index=final_hexs, crs="EPSG:4326")
+    all_polys = gpd.GeoDataFrame(all_polys, crs=4326, columns=['geometry'])
+    all_polys['shape_id'] = list(all_polys.index)
+    return(all_polys)
+    
+def map_choropleth(sub, map_column, thresh=[], colour_ramp = 'Reds', invert=False, map_epsg=3857, legend_loc='upper right'):
+        ''' generate a static map of variables in GeoDataFrame sub
+            
+            :param sub: GeoDataFrame with geometry and column to map
+            :type sub: GeoPandas.GeoDataFrame
+            :param map_column: Name of column in sub to map
+            :type map_column: string
+            :param thresh: list of values to classify data in map_column
+            :type thresh: list of ints
+        '''
+        try:
+            sub = sub.to_crs(map_epsg)
+        except:
+            sub.crs = 4326
+            sub = sub.to_crs(map_epsg)
+
+        thresh=[]
+        map_sub = sub.copy()
+        cmap = matplotlib.cm.get_cmap(colour_ramp)
+        fig, ax = plt.subplots(figsize=(15,15))
+        proj = CRS.from_epsg(map_epsg)
+
+        # create map column in sub, based on re-mapping of column map_column
+        if len(thresh) == 0:
+            split = [0,0.2,0.4,0.6,0.8,1]
+            thresh = [x for x in map_sub[map_column].quantile(split).values]
+            thresh.insert(0,0)
+
+        map_sub['map'] = pd.cut(map_sub[map_column], thresh, labels=list(range(0, len(thresh)-1)))
+
+        # [x/max(thresh) for x in thresh]
+        cmap_divisions = [x/100 for x in list(range(0,101,20))]
+        # map features not included in grouping
+        sel_mixed = map_sub.loc[map_sub['map'].isna()]
+        mismatch_color = 'azure'
+        mismatch_edge = 'darkblue'
+        cur_patch = mpatches.Patch(facecolor=mismatch_color, edgecolor=mismatch_edge, hatch="///", label=f"Mismatch [{sel_mixed.shape[0]}]")
+        all_labels = [cur_patch]
+        for lbl, data in map_sub.groupby('map'):
+            cur_color = cmap(cmap_divisions[int(lbl)])
+            if invert:
+                cur_color = cmap(1 - cmap_divisions[int(lbl)])
+            data.plot(color=cur_color, ax=ax, linewidth=0.1)
+            cur_patch = mpatches.Patch(color=cur_color, label=f'{data[map_column].min()} - {data[map_column].max()} [{data.shape[0]}]')
+            all_labels.append(cur_patch)
+
+        sel_mixed.plot(color=mismatch_color, edgecolor=mismatch_edge, hatch="//////", ax=ax, label=False, linewidth=2)
+            
+        ctx.add_basemap(ax, source=ctx.providers.Stamen.TonerBackground, crs=proj) #zorder=-10, 'EPSG:4326'
+        ax.legend(handles=all_labels, loc=legend_loc)        
+        ax = ax.set_axis_off()
+
+        return(ax)
+    
+def static_map_h3(sub, map_epsg=3857, legend_loc='upper right'):
+        ''' generate a static map of the h3 grid in sub
+        '''
+        try:
+            sub = sub.to_crs(map_epsg)
+        except:
+            sub.crs = 4326
+            sub = sub.to_crs(map_epsg)
+        
+        fig, ax = plt.subplots(figsize=(15,15))
+        proj = CRS.from_epsg(map_epsg)
+        
+        sub.plot(color='grey', ax=ax, linewidth=0.1)
+            
+        ctx.add_basemap(ax, source=ctx.providers.Stamen.TonerBackground, crs=proj) #zorder=-10, 'EPSG:4326'
+        ax = ax.set_axis_off()
+        return(ax)
