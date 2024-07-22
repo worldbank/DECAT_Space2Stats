e9254b2ca1182eb90169ee40063ec57679a19939
Zachary Deziel <zachary.deziel@gmail.com>
2024-07-22 10:38:10 -0700

    Update ingestion scripts

--- postgres/chunk_parquet.py
+++ postgres/chunk_parquet.py
@@ -1,11 +1,16 @@
+import os
+
 import pandas as pd
 
-
-df = pd.read_parquet('space2stats.parquet')
+chunk_dir = "parquet_chunks"
+df = pd.read_parquet('space2stats_updated.parquet')
 chunk_size = 100000  # Number of rows per chunk
+
+if not os.path.exists(chunk_dir):
+    os.mkdir(chunk_dir)
 
 for i in range(0, len(df), chunk_size):
     chunk = df.iloc[i:i + chunk_size]
-    chunk.to_parquet(f'parquet_chunks/space2stats_part_{i // chunk_size}.parquet')
+    chunk.to_parquet(os.path.join(chunk_dir, f'space2stats_part_{i // chunk_size}.parquet'))
 
 print("Parquet file split into smaller chunks.")
