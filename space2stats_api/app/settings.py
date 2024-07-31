e4093baf7b454be537fac6265774495c7765d2f0
Zachary Deziel <zachary.deziel@gmail.com>
2024-07-31 10:40:56 -0700

    Add pytest to pre-commit
    Required adding relative path handling to settings.py to manage running uvicorn/pytest from the space2stats_api directory but also support running pre-commit from the root.

--- space2stats_api/app/settings.py
+++ space2stats_api/app/settings.py
@@ -1,5 +1,15 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
+current_directory = os.getcwd()    
+env_file_name = 'local_db.env'
+
+if os.path.basename(current_directory) == "space2stats_api":
+    env_path = os.path.join(current_directory, env_file_name)
+else:
+    env_path = os.path.join(current_directory, env_file_name)
 
 class Settings(BaseSettings):
     DB_HOST: str
@@ -8,8 +18,8 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_TABLE_NAME: str
-
-    model_config = SettingsConfigDict(env_file="../local_db.env")
+        
+    model_config = SettingsConfigDict(env_file=env_path)
 
 
 settings = Settings()
