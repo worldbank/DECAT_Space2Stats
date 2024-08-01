f9eeb586f3fe2c04b51197d43858b68b1f244e04
Zachary Deziel <zachary.deziel@gmail.com>
2024-08-01 07:45:49 -0700

    Update from feedback on PR
    Remove execution of tests on pre-commit. Remove current directory check for setting path of env file.

--- space2stats_api/app/settings.py
+++ space2stats_api/app/settings.py
@@ -1,15 +1,5 @@
-import os
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
-current_directory = os.getcwd()    
-env_file_name = 'local_db.env'
-
-if os.path.basename(current_directory) == "space2stats_api":
-    env_path = os.path.join(current_directory, '..', env_file_name)
-else:
-    env_path = os.path.join(current_directory, env_file_name)
 
 class Settings(BaseSettings):
     DB_HOST: str
@@ -19,7 +9,7 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_TABLE_NAME: str
         
-    model_config = SettingsConfigDict(env_file=env_path)
+    model_config = SettingsConfigDict(env_file='../db.env')
 
 
 settings = Settings()
