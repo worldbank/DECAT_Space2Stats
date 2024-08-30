4b51bb4871ce2f567fef0b3790f892a2afb079aa
Vincent Sarago <vincent.sarago@gmail.com>
2024-08-31 01:06:25 +0200

    refactor to ease usage and packaging (#32)
    * refactor to ease usage and packaging
    
    * Update .gitignore
    
    * add open=True to avoid deprecation
    
    * add compression middleware
    
    * switch to orjson
    
    * Use poetry for dependency management given lambda deployment
    
    * Update CI for poetry dependency management
    
    * Add pre-commit to poetry test dependencies
    
    * Run pip through python
    
    ---------
    
    Co-authored-by: Zachary Deziel <zachary.deziel@gmail.com>

---
+++ space2stats_api/src/space2stats/app.py
@@ -0,0 +1,60 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import ORJSONResponse
+
+from typing import Any, Dict, List
+
+from starlette.requests import Request
+from starlette_cramjam.middleware import CompressionMiddleware
+
+from .db import connect_to_db, close_db_connection
+from .main import get_summaries_from_geom, get_available_fields, SummaryRequest
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """FastAPI Lifespan."""
+    # Create Connection Pool
+    await connect_to_db(app)
+    yield
+    # Close the Connection Pool
+    await close_db_connection(app)
+
+
+app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(CompressionMiddleware)
+
+
+@app.post("/summary", response_model=List[Dict[str, Any]])
+def get_summary(request: Request, body: SummaryRequest):
+    with request.app.state.pool.connection() as conn:
+        return get_summaries_from_geom(
+            body.aoi,
+            body.spatial_join_method,
+            body.fields,
+            conn,
+            geometry=body.geometry,
+        )
+
+
+@app.get("/fields", response_model=List[str])
+def fields(request: Request):
+    with request.app.state.pool.connection() as conn:
+        return get_available_fields(conn)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Space2Stats!"}
---
+++ space2stats_api/src/space2stats/db.py
@@ -0,0 +1,40 @@
+"""Database connection handling."""
+
+from typing import Any, Dict, Optional
+
+from fastapi import FastAPI
+from psycopg_pool import ConnectionPool
+
+from .settings import Settings
+
+
+async def connect_to_db(
+    app: FastAPI,
+    settings: Optional[Settings] = None,
+    pool_kwargs: Optional[Dict[str, Any]] = None,
+) -> None:
+    """Connect to Database."""
+    if not settings:
+        settings = Settings()
+
+    pool_kwargs = pool_kwargs or {}
+
+    app.state.pool = ConnectionPool(
+        conninfo=settings.DB_CONNECTION_STRING,
+        min_size=settings.DB_MIN_CONN_SIZE,
+        max_size=settings.DB_MAX_CONN_SIZE,
+        max_waiting=settings.DB_MAX_QUERIES,
+        max_idle=settings.DB_MAX_IDLE,
+        num_workers=settings.DB_NUM_WORKERS,
+        kwargs=pool_kwargs,
+        open=True,
+    )
+
+    # Make sure the pool is ready
+    # ref: https://www.psycopg.org/psycopg3/docs/advanced/pool.html#pool-startup-check
+    app.state.pool.wait()
+
+
+async def close_db_connection(app: FastAPI) -> None:
+    """Close Pool."""
+    app.state.pool.close()
