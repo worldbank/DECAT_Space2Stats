c3d3413ad1021d9e5a5c69eb350632d17ed1baa9
Zachary Deziel <zachary.deziel@gmail.com>
2024-09-19 09:20:13 -0700

    Remove unused files and database configuration

--- postgres/deploy.md
+++
@@ -1,7 +0,0 @@
-## Deployment Notes
-
-- Create database instance
-- Update configuration in `db.env`
-- Ingest parquet file with `load_to_prod.sh` (may require `chmod +x load_to_prod.sh`)
-- Create index on hex_id (for performance):`CREATE INDEX idx_hex_id ON space2stats (hex_id)` - critical for performance of our queries
-- Test with the [example notebook](notebooks/space2stats_api_demo.ipynb)
--- postgres/download_parquet.sh
+++ postgres/download_parquet.sh
@@ -8,13 +8,6 @@ S3_BUCKET="wbg-geography01"
 PARQUET_FILE="Space2Stats/parquet/GLOBAL/combined_population.parquet"
 LOCAL_PARQUET_FILE="space2stats.parquet"
 
-# PostgreSQL configuration
-PGHOST="${MY_DOCKER_IP:-127.0.0.1}"
-PGPORT=5439
-PGDATABASE="postgis"
-PGUSER="username"
-PGPASSWORD="password"
-
 # Download Parquet file from S3
 echo "Downloading Parquet file from S3..."
 aws s3 cp --quiet s3://$S3_BUCKET/$PARQUET_FILE $LOCAL_PARQUET_FILE
