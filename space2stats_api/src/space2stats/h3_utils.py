a32650d350147eb2d67cdf1a13f9913debe4522b
Zachary Deziel <zachary.deziel@gmail.com>
2024-10-03 14:17:34 -0700

    Fix issue with Point generation
    h3ronpy currently has a bug with cells_to_wkb_points which returns matching coordinates.

--- space2stats_api/src/space2stats/h3_utils.py
+++ space2stats_api/src/space2stats/h3_utils.py
@@ -2,13 +2,13 @@ from typing import Any, Dict, List, Literal
 
 import pyarrow as pa
 from h3ronpy import ContainmentMode
-from h3ronpy.arrow.vector import (
-    cells_to_wkb_points,
+from h3ronpy.arrow.vector import (  # Import cells_to_coordinates for point geometries
+    cells_to_coordinates,
     cells_to_wkb_polygons,
     geometry_to_cells,
 )
 from shapely import wkb
-from shapely.geometry import mapping, shape
+from shapely.geometry import Point, mapping, shape
 
 CONTAINMENT_MODE_MAP = {
     "centroid": ContainmentMode.ContainsCentroid,
@@ -43,37 +43,36 @@ def generate_h3_ids(
 def generate_h3_geometries(
     h3_ids_uint64: List[int], geometry_type: Literal["polygon", "point"] = "polygon"
 ) -> List[Dict]:
-    """
-    Generate geometries (GeoJSON polygons or points) for a list of H3 hexagon IDs (as uint64).
-
-    Parameters:
-        h3_ids_uint64 (List[int]): A list of H3 hexagon IDs in uint64 format.
-        geometry_type (Optional[str]):
-            The type of geometry to generate ('polygon' or 'point').
-
-    Returns:
-        List[Dict]: A list of GeoJSON geometries (Polygon or Point).
-    """
     if geometry_type == "polygon":
-        # Convert H3 cells to WKB polygons
-        wkb_geometries = cells_to_wkb_polygons(h3_ids_uint64)
+        return _get_polygon(h3_ids_uint64)
     elif geometry_type == "point":
-        # Convert H3 cells to WKB points (centroid of each hexagon)
-        wkb_geometries = cells_to_wkb_points(h3_ids_uint64)
+        return _get_point(h3_ids_uint64)
     else:
         raise ValueError("Invalid geometry type. Use 'polygon' or 'point'.")
 
-    # Convert pyarrow Array to GeoJSON geometries
+
+def _get_polygon(h3_ids_uint64: List[int]) -> List[Dict]:
+    wkb_geometries = cells_to_wkb_polygons(h3_ids_uint64)
     geojson_geometries = []
     for geom in wkb_geometries:
-        if isinstance(geom, pa.LargeBinaryScalar):
-            # Convert the LargeBinaryScalar to bytes
+        if isinstance(geom, pa.LargeBinaryScalar) or isinstance(geom, pa.BinaryScalar):
             shapely_geom = wkb.loads(geom.as_py())
-            # Convert Shapely geometry to GeoJSON-like dict
-            geojson_geometries.append(mapping(shapely_geom))
+            geojson_geom = mapping(shapely_geom)
+            geojson_geometries.append(geojson_geom)
         else:
             raise TypeError(
-                f"Expected pa.LargeBinaryScalar, but got {type(geom).__name__}"
+                f"Expected pa.LargeBinaryScalar or pa.BinaryScalar, but got {type(geom).__name__}"
             )
+    return geojson_geometries
 
+
+def _get_point(h3_ids_uint64: List[int]) -> List[Dict]:
+    coords_table = cells_to_coordinates(h3_ids_uint64)
+    latitudes = coords_table["lat"].to_numpy()
+    longitudes = coords_table["lng"].to_numpy()
+
+    geojson_geometries = []
+    for lat, lon in zip(latitudes, longitudes):
+        point_geom = Point(lon, lat)
+        geojson_geometries.append(mapping(point_geom))
     return geojson_geometries
