7fab53e8c54789561fd3038cdf3b000577300146
gabe-levin <glev2190@gmail.com>
2024-10-30 13:03:07 +0100

    refactor: stac_columns test to compare with EXCEL rather than types.json

--- space2stats_api/src/tests/metadata_tests/test_stac_columns.py
+++ space2stats_api/src/tests/metadata_tests/test_stac_columns.py
@@ -1,10 +1,19 @@
 import json
 
+import pandas as pd
 
-def test_stac_columns_vs_types_json(stac_file_path, types_json_file_path):
-    # Load the expected column types from the types JSON file
-    with open(types_json_file_path, "r") as f:
-        expected_columns = json.load(f)
+
+def test_stac_columns_vs_types_json(stac_file_path, metadata_excel_file_path):
+    # Load the expected column types from the Metadata Content Excel
+    feature_catalog = pd.read_excel(
+        metadata_excel_file_path, sheet_name="Feature Catalog"
+    )
+    expected_columns = feature_catalog[feature_catalog["source"] == "Population"]
+
+    # Convert the DataFrame to a dictionary for easier comparison
+    expected_columns_dict = dict(
+        zip(expected_columns["variable"], expected_columns["type"])
+    )
 
     # Load the STAC item from the JSON file
     with open(stac_file_path, "r") as f:
@@ -15,16 +24,16 @@ def test_stac_columns_vs_types_json(stac_file_path, types_json_file_path):
         col["name"]: col["type"] for col in stac_item["properties"]["table:columns"]
     }
 
-    # Assert that the number of columns in the STAC file matches the number of columns in the types JSON file
+    # Assert that the number of columns in the STAC file matches the number of columns in the types TABLE file
     assert (
-        len(stac_columns) == len(expected_columns)
-    ), f"Mismatch in column count: STAC ({len(stac_columns)}) vs JSON ({len(expected_columns)})"
+        len(stac_columns) == len(expected_columns_dict)
+    ), f"Mismatch in column count: STAC ({len(stac_columns)}) vs TABLE ({len(expected_columns_dict)})"
 
     # Assert that column names and types match
-    for column_name, column_type in expected_columns.items():
+    for column_name, column_type in expected_columns_dict.items():
         assert (
             column_name in stac_columns
         ), f"Column {column_name} is missing in the STAC file"
         assert (
             stac_columns[column_name] == column_type
-        ), f"Mismatch in column type for {column_name}: STAC ({stac_columns[column_name]}) vs JSON ({column_type})"
+        ), f"Mismatch in column type for {column_name}: STAC ({stac_columns[column_name]}) vs TABLE ({column_type})"
