000fc2d9b0a496071567ab3d3b508c2abe15ef66
Anthony Lukach <anthonylukach@gmail.com>
2024-11-05 11:26:28 -0800

    Add tooling for PR preview URL

--- space2stats_api/cdk/settings.py
+++ space2stats_api/cdk/settings.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic_settings import BaseSettings
 
 
@@ -14,5 +15,5 @@ class DeploymentSettings(BaseSettings):
     CDK_DEFAULT_ACCOUNT: str
     CDK_DEFAULT_REGION: str
     CDK_CERTIFICATE_ARN: str
-    CDK_DOMAIN_NAME: str
+    CDK_DOMAIN_NAME: Optional[str]
     STAGE: str = "dev"
