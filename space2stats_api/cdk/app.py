14281fe9aeddad8f287b18e5ba754c913ebc900e
Zac Deziel <zachary.deziel@gmail.com>
2024-11-07 09:25:01 -0800

    Feature/cd (#78)
    * Update adding staging mechanism
    
    * Add CD pipeline
    
    * Solve f-string matching
    
    * Update event to push on main
    
    * Add dev CD
    
    * Remove npm and node specific jobs
    
    * Update variables
    
    * Update environment
    
    * Update role
    
    * Remove cdk bootstrap
    
    * Add working directory for cdk deployment
    
    * Add cdk requirements
    
    * Update cdk requirements
    
    * Update deployment variables
    
    * Refactor deployment tooling (#85)
    
    * Migrate to reusable workflow
    
    * Add tooling for PR preview URL
    
    * Add runs-on
    
    * Pre-commit
    
    * Refactor triggers
    
    * Bump version
    
    * Add permissions
    
    * Fix working dir
    
    * Fix URL output
    
    * Fix comment find
    
    * Add tooling to tear down PR preview
    
    * Fix commenting
    
    * Fix
    
    * Run tests on all pushes
    
    * Refactor
    
    * Fix
    
    * Expand events
    
    * Fix destroy
    
    * Prevent unnecessary deployments
    
    * Pass in PR number
    
    * Prevent testing on closed PRs
    
    * Refactor
    
    * Mv to workflows
    
    * Fix destroy
    
    * Rework trigger
    
    * Refine
    
    * Rm old needs
    
    * Rework trigger
    
    * Try fix if condition
    
    * Mv deployment trigger back to ci.yml
    
    * Rework triggers
    
    * Rename jobs
    
    * Set concurrency to stage
    
    * Only trigger on pushes to main
    
    ---------
    
    Co-authored-by: Anthony Lukach <anthonylukach@gmail.com>

--- space2stats_api/cdk/app.py
+++ space2stats_api/cdk/app.py
@@ -1,8 +1,12 @@
+import os
+
 from aws_cdk import App, Environment
 from aws_stack import Space2StatsStack
 from settings import DeploymentSettings
 
-settings = DeploymentSettings(_env_file="aws_deployment.env")
+settings = DeploymentSettings(
+    _env_file=f"aws_deployment_{os.environ.get('STAGE', 'dev')}.env"
+)
 
 env = Environment(
     account=settings.CDK_DEFAULT_ACCOUNT, region=settings.CDK_DEFAULT_REGION
@@ -10,6 +14,8 @@ env = Environment(
 
 app = App()
 
-Space2StatsStack(app, "Space2StatsStack", env=env)
+Space2StatsStack(
+    app, f"Space2Stats-{settings.STAGE}", env=env, deployment_settings=settings
+)
 
 app.synth()
