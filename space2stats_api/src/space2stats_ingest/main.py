bcf7098e9066a42e06239ed9865160c1fa990fd2
gabe-levin <glev2190@gmail.com>
2024-11-26 18:25:50 +0100

    fix: include parquet hex_id in verify columns, STAC hex_id need to be standarized

--- space2stats_api/src/space2stats_ingest/main.py
+++ space2stats_api/src/space2stats_ingest/main.py
@@ -55,7 +55,9 @@ def verify_columns(
         raise ValueError("The 'hex_id' column is missing from the Parquet file.")
 
     # Verify Parquet columns match the STAC fields
-    if parquet_columns - {"hex_id"} != stac_fields:
+    # TODO: Standarize the hex_id in the parquet files/STAC items.
+    # if parquet_columns - {"hex_id"} != stac_fields:
+    if parquet_columns != stac_fields:
         extra_in_parquet = parquet_columns - stac_fields
         extra_in_stac = stac_fields - parquet_columns
         raise ValueError(
