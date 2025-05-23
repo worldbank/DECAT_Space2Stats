be7dfe092104f9659c088df34232fde8ccc6b3a6
gabe-levin <glev2190@gmail.com>
2025-05-23 15:25:21 +0200

    fix: elevating version to 1.3.0

--- space2stats_client/src/space2stats_client/__init__.py
+++ space2stats_client/src/space2stats_client/__init__.py
@@ -2,6 +2,6 @@
 
 from .client import Space2StatsClient
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 __license__ = "World Bank Master Community License Agreement"
 __copyright__ = "Copyright (c) 2025 The World Bank"
